b6fa1b81b73a278545640125a838602f88049c58
tinylin <521017853@qq.com>
2018-07-05 01:06:18 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <div id="content">
+            <div class="inner">
+                <router-view></router-view>
+            </div>
+        </div>
+
+        <!-- Sidebar -->
+        <div id="sidebar">
+            <!-- Logo -->
+            <h1 id="logo"><a href="/">QQ音乐前端博客</a></h1>
+
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <!-- current -->
+                    <!-- <li v-for="item in columns" :key="item.Fname" :class="$route.params.id == item.Fid ? 'current' : ''">
+                        <router-link v-bind:to="item.Fid == '/' ? '/' : `/column/${item.Fid}`">{{item.Fname}}</router-link>
+                    </li> -->
+                </ul>
+            </nav>
+
+            <!-- Search -->
+            <!-- <section class="box search">
+                <form method="post" action="#">
+                    <input type="text" class="text" name="search" placeholder="Search" />
+                </form>
+            </section> -->
+
+            <!-- Copyright -->
+            <ul id="copyright">
+                <li><router-link to="/123">&copy; QQMusic Web</router-link></li>
+            </ul>
+        </div>
+    </div>
+</template>
---
+++ src/views/ListView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="list">
+        <article class="box post post-excerpt" v-for="item in articles" :key="item.title">
+            <header>
+                <h2><router-link v-bind:to="'/data'">{{item.Ftitle}}</router-link></h2>
+                <p>{{item.Fauthor}} {{item.Ftime  }}</p>
+            </header>
+            
+            <img class="image featured" v-if="item.Fimg" :src="item.Fimg" alt="" />
+            <p v-if="item.Fdesc">{{item.Fdesc}}</p>
+        </article>
+    </div>
+</template>
+
+<script>
+import '../css/main.css';
+export default {
+    computed: {
+        articles () {
+            return this.$store.state.articles;
+        }
+    },
+    asyncData ({ store, route: { params: { id } } }) {
+        return store.dispatch('FETCH_ARTICLE', { ids: [id] })
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
